3a122c6b9fe7a07d6ff11e5da15d2f9b5eb69b57
erickmo <erick.mo@vortege.com>
2015-10-14 23:49:34 +0700

    add analytics

--- resources/views/web/v1/templates/tmp.blade.php
+++ resources/views/web/v1/templates/tmp.blade.php
@@ -50,5 +50,15 @@
 
 		{!! Html::script(elixir('js/jquery-2.1.4.min.js')) !!}
 		{!! Html::script(elixir('js/bootstrap.min.js')) !!}
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-68128588-2', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
 	</body>
 </html>
